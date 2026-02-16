d879f54a26595268fdfe94844e5d6d8e53c86541
tylerkilburn <tylerkilburn@gmail.com>
2026-02-16 13:58:50 -0500

    Manual update

--- qmk/features/process_record_user.c
+++ qmk/features/process_record_user.c
@@ -3,6 +3,8 @@
 
 static uint16_t alt_sent_timer;
 
+static uint16_t alt_sent_timer;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case CUST_ALT_QUE:
@@ -10,27 +12,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       alt_sent_timer = timer_read();
       register_mods(MOD_BIT(KC_LALT));
     } else {
-      // 1. Unregister Alt and FORCE a report to the computer
+      // 1. Kill Alt and force the computer to see the release
       unregister_mods(MOD_BIT(KC_LALT));
       send_keyboard_report();
 
       if (timer_elapsed(alt_sent_timer) < TAPPING_TERM) {
-        // 2. Add a tiny 10ms delay to let the OS process the Alt release
-        wait_ms(10);
+        // 2. Send a "Dummy" key to stop the OS from highlighting the Alt menu
+        tap_code(KC_F24);
 
-        // 3. Capture Shift state then clear all mods
+        // 3. Clean up all mod states for a pure symbol tap
         uint8_t temp_mods = get_mods();
         clear_mods();
-        clear_weak_mods(); // Crucial: clears QMK's internal shift state
+        clear_weak_mods();
         send_keyboard_report();
 
+        // 4. Send the symbol based on Shift state
         if (temp_mods & MOD_MASK_SHIFT) {
           tap_code16(KC_EXLM);
         } else {
           tap_code16(KC_QUES);
         }
 
-        // 4. Restore Shift if you were still holding it
+        // 5. Restore your physical mods (like Shift)
         set_mods(temp_mods);
         send_keyboard_report();
       }
