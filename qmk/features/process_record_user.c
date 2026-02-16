c028862b73fdba639bdb7d4816aecc124a1daa36
tylerkilburn <tylerkilburn@gmail.com>
2026-02-16 12:50:08 -0500

    Manual update

--- qmk/features/process_record_user.c
+++ qmk/features/process_record_user.c
@@ -1,20 +1,19 @@
+
 #include "process_record_user.h"
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case CUST_ALT_QUE:
     if (record->event.pressed) {
-      // Act as Alt immediately on press
+      // Start timer to distinguish tap vs hold
+      record->tap.interrupted = false;
       register_mods(MOD_BIT(KC_LALT));
     } else {
-      // Release Alt on release
       unregister_mods(MOD_BIT(KC_LALT));
-
-      // If it's a tap, decide which character to send
-      if (record->tap.count > 0) {
-        if (get_mods() & MOD_MASK_SHIFT) {
+      // If it was a tap (not held long enough to be Alt)
+      if (record->tap.count > 0 && !record->tap.interrupted) {
+        if (get_mods() & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) {
           // Shift is held: send '!'
-          // tap_code16 handles the internal shift state for you
           tap_code16(KC_EXLM);
         } else {
           // No shift: send '?'
@@ -22,7 +21,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       }
     }
-    return false; // Don't process this key further
+    return false;
   }
   return true;
-}
+};
