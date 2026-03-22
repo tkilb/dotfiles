3cf6ff5e63e17e6e4c8405243981702f98749385
tylerkilburn <tylerkilburn@gmail.com>
2026-03-22 01:01:29 -0500

    Hypr configs to login

--- hypr/arrange.sh
+++ hypr/arrange.sh
@@ -2,9 +2,11 @@
 
 # Config: app_class -> workspace mappings (add/remove lines as needed)
 declare -A APP_WS=(
-    ["kitty"]=3
-    ["zen"]=2
-    ["bitwarden"]=6
+  ["kitty"]=3
+  ["zen"]=2
+  ["Bitwarden"]=5
+  ["Discord"]=5
+  ["steam"]=6
 )
 
 # Save the currently focused window
@@ -12,11 +14,11 @@ focused_addr=$(hyprctl activewindow -j | jq -r '.address')
 
 # Move all configured app windows to their designated workspaces
 for app_class in "${!APP_WS[@]}"; do
-    ws="${APP_WS[$app_class]}"
-    for addr in $(hyprctl clients -j | jq -r --arg class "$app_class" '.[] | select(.class == $class or .class == ($class | ascii_upcase)) | .address'); do
-        hyprctl dispatch focuswindow "address:$addr"
-        hyprctl dispatch movetoworkspacesilent "$ws"
-    done
+  ws="${APP_WS[$app_class]}"
+  for addr in $(hyprctl clients -j | jq -r --arg class "$app_class" '.[] | select((.class | ascii_downcase) == ($class | ascii_downcase)) | .address'); do
+    hyprctl dispatch focuswindow "address:$addr"
+    hyprctl dispatch movetoworkspacesilent "$ws"
+  done
 done
 
 # Restore focus to the original window
