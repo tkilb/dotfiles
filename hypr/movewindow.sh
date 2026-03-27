972b499ad10c86d32ff76baad8ebe129770115ac
tyler <tylerkilburn@gmail.com>
2026-03-26 21:23:02 -0400

    feat: hyprland tweaks

--- hypr/movewindow.sh
+++ hypr/movewindow.sh
@@ -7,25 +7,29 @@
 direction=$1
 workspace=$2
 
-# Get current window address
-pre=$(hyprctl -j activewindow | jq -r ".address")
+# Get current window position
+get_pos() {
+  hyprctl -j activewindow | jq -c ".at"
+}
+
+pre=$(get_pos)
 
 # Try to move window
 hyprctl dispatch movewindow "$direction"
 
-# Get new window address
-post=$(hyprctl -j activewindow | jq -r ".address")
+# Get new window position
+post=$(get_pos)
 
-# If window didnt move, move it to the next workspace
-if [[ $post = $pre ]]; then
+# If window didn't move, move it to the next workspace
+if [[ "$post" == "$pre" ]]; then
   if [[ $workspace == "r+1" ]]; then
     current_workspace=$(hyprctl activeworkspace -j | jq -r ".id")
     new_workspace=$(( (current_workspace % 6) + 1 ))
-    hyprctl dispatch movetoworkspace $new_workspace
+    hyprctl dispatch movetoworkspace "$new_workspace"
   elif [[ $workspace == "r-1" ]]; then
     current_workspace=$(hyprctl activeworkspace -j | jq -r ".id")
     new_workspace=$(( (current_workspace + 4) % 6 + 1 ))
-    hyprctl dispatch movetoworkspace $new_workspace
+    hyprctl dispatch movetoworkspace "$new_workspace"
   else
     hyprctl dispatch movetoworkspace "$workspace"
   fi
