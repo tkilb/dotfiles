ed8e26a3cb6f5ba8d8721fee2acd1de9dfc0c0bb
Tyler Kilburn <tylerkilburn@carfax.com>
2026-06-10 10:27:24 -0400

    feat: updates aerospace

--- aerospace/scripts/aerospace-arrange.sh
+++ aerospace/scripts/aerospace-arrange.sh
@@ -8,6 +8,7 @@ aerospace list-windows --all | awk -F' *\\| *' '
     /Slack/             { print $1, 4 }
     /Bitwarden/         { print $1, 5 }
     /Gather/            { print $1, 6 }
+    /Figma/            { print $1, 6 }
     /^Zoom/             { print $1, "B" }
     /Microsoft Outlook/ { print $1, "C" }
 ' | while read -r window_id workspace; do
