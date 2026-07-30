67f9ca4cbbaa62e036a86f54e2a9513775f2d246
tylerkilburn <tylerkilburn@gmail.com>
2026-07-30 18:19:11 -0400

    feat: adds openscad plugin to nvim

---
+++ nvim/lua/plugins/open-scad.lua
@@ -0,0 +1,19 @@
+-- Name: Openscad
+--
+-- Docs: https://github.com/salkin-mada/openscad.nvim
+--
+-- Description:
+--   Syntax highlighting, cheatsheet, snippets, offline manual and fuzzy help plugin for the OpenSCAD language.
+
+return {
+  "salkin-mada/openscad.nvim",
+  config = function()
+    vim.g.openscad_load_snippets = true
+    vim.g.openscad_pdf_cmd = "zathura"
+    require("openscad")
+  end,
+  dependencies = {
+    "ibhagwan/fzf-lua",
+    "L3MON4D3/LuaSnip", -- optional
+  },
+}
